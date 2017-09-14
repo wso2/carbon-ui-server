9674f6dbe8759aeeec66004a7cbcc74bdb687892
Sajith Janaprasad <sajithar@wso2.com>
2017-09-14 09:12:01 +0530

    remove AppFinder usage from AppRegistry class

--- components/core/src/main/java/org/wso2/carbon/uis/internal/deployment/AppRegistry.java
+++ components/core/src/main/java/org/wso2/carbon/uis/internal/deployment/AppRegistry.java
@@ -18,89 +18,79 @@
 
 package org.wso2.carbon.uis.internal.deployment;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.commons.lang3.tuple.Pair;
 import org.wso2.carbon.uis.api.App;
 import org.wso2.carbon.uis.internal.exception.AppCreationException;
-import org.wso2.carbon.uis.internal.exception.DeploymentException;
-import org.wso2.carbon.uis.internal.exception.FileOperationException;
 import org.wso2.carbon.uis.internal.reference.AppReference;
 
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 /**
- * A registry that maintains deployed web apps.
+ * A registry that holds created or to-be-created web apps.
  *
- * @since 1.0.0
+ * @since 0.8.0
  */
 public class AppRegistry {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(AppRegistry.class);
-
-    private final AppFinder appFinder;
-    private final ConcurrentMap<String, App> deployedApps;
+    private final ConcurrentMap<String, App> createdApps = new ConcurrentHashMap<>();
+    private final ConcurrentMap<String, Pair<AppReference, String>> pendingToCreateApps = new ConcurrentHashMap<>();
 
     /**
-     * Creates a new app registry with the specified app finder and app creator.
+     * Adds a new web app to this registry.
      *
-     * @param appFinder  app finder to be used
+     * @param appReference reference to the app
      */
-    public AppRegistry(AppFinder appFinder) {
-        this.appFinder = appFinder;
-        this.deployedApps = new ConcurrentHashMap<>();
+    public void addApp(AppReference appReference, String appContextPath) {
+        pendingToCreateApps.put(appReference.getName(), Pair.of(appReference, appContextPath));
     }
 
     /**
-     * Returns names and context paths of available apps.
+     * Removes the specified web app from this registry.
      *
-     * @return names and context paths of available apps
+     * @param appName name of the app to be removed
      */
-    public Map<String, String> getAvailableApps() {
-        return appFinder.getAvailableApps();
-    }
-
-    /**
-     * Returns the app for the given context path.
-     *
-     * @param appContextPath app's context path
-     * @return if present the app for the given context path, otherwise {@code null}
-     * @throws DeploymentException if some error occurred during app deployment
-     */
-    public App getApp(String appContextPath) throws DeploymentException {
-        return deployedApps.computeIfAbsent(appContextPath, this::createApp);
-    }
-
-    /**
-     * Clears all the deployed apps of this registry.
-     */
-    public void clear() {
-        deployedApps.clear();
-    }
-
-    private App createApp(String appContextPath) throws DeploymentException {
-        AppReference appReference;
-        try {
-            appReference = appFinder.getAppReference(appContextPath).orElse(null);
-        } catch (FileOperationException e) {
-            throw new DeploymentException("Cannot load an app for context path '" + appContextPath + "'.", e);
+    public void removeApp(String appName) {
+        if (createdApps.remove(appName) == null) {
+            pendingToCreateApps.remove(appName);
         }
-        if (appReference == null) {
-            return null; // no app found for the given context path
+    }
+
+    /**
+     * Returns the created web app of the specified name. If the app is not created previously, then it will be created
+     * and returned.
+     *
+     * @param appName name of the app
+     * @return created app, or {@code null} if there is no app found for the given name
+     * @throws AppCreationException if an error occurred during app creation
+     */
+    public App getApp(String appName) throws AppCreationException {
+        return createdApps.computeIfAbsent(appName, this::createApp);
+    }
+
+    /**
+     * Returns already created web apps in this registry.
+     *
+     * @return created apps
+     */
+    public Set<App> getCreatedApps() {
+        return new HashSet<>(createdApps.values());
+    }
+
+    private App createApp(String appName) throws AppCreationException {
+        Pair<AppReference, String> appReferenceContextPath = pendingToCreateApps.get(appName);
+        if (appReferenceContextPath == null) {
+            return null;
         }
 
-        App app;
         try {
-            app = AppCreator.createApp(appReference, appContextPath);
-        } catch (AppCreationException | FileOperationException e) {
-            throw new DeploymentException("Cannot create app '" + appReference.getName() +
-                                          "' to deploy for context path '" + appContextPath + "'.", e);
+            return AppCreator.createApp(appReferenceContextPath.getLeft(), appReferenceContextPath.getRight());
         } catch (Exception e) {
-            throw new DeploymentException(
-                    "Cannot deploy app '" + appReference.getName() + "' for context path '" + appContextPath + "'.", e);
+            throw new AppCreationException(
+                    "Cannot create app '" + appReferenceContextPath.getLeft().getName() +
+                    "' to deploy for context path '" + appReferenceContextPath.getRight() + "'.", e);
         }
-        LOGGER.debug("Web app '{}' deployed for context path '{}' successfully.", app.getName(), app.getContextPath());
-        return app;
     }
 }
